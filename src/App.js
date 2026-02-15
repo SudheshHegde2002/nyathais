5b54ac080d3f4c08c6122a1ad4b75545ffdbd159
Sudhesh Hegde <hegdesud2002@gmail.com>
2026-02-15 19:16:44 +0530

    Added sample gif's

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="gif-container">
+        <img
+          src={process.env.PUBLIC_URL + '/Comp_setup_composition_202602151359.gif'}
+          alt="Composition Setup GIF"
+          className="gif-item"
+        />
+        <img
+          src={process.env.PUBLIC_URL + '/kling_20260214_VIDEO_Create_a_s_1113_01-ezgif.com-video-to-gif-converter.gif'}
+          alt="Kling Video GIF"
+          className="gif-item"
+        />
+      </div>
     </div>
   );
 }
